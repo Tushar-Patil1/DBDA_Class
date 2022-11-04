dcd8ab5c1930f615bf462e27a93aa4fba8ea31c3
Tushar Shirsath <114068405+Tushar-Patil1@users.noreply.github.com>
2022-11-05 00:41:10 +0530

    Add files via upload

---
+++ java/day_02/Sample.java
@@ -0,0 +1,13 @@
+
+public class Sample
+{
+	public static void main(String[] args)
+
+	{
+		System.out.println("Something printing");	
+		System.out.println("Enter 1 st no: ");
+		System.out.println("Result is "+c);	
+	}
+
+}
+
